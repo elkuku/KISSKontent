f5954f9890c571954b3b3e7bfe00a561c1817f98
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-09 13:06:22 -0500

    First import

---
+++ plugin/kukukontent/kukukontent.php
@@ -0,0 +1,36 @@
+<?php
+
+// No direct access allowed to this file
+defined('_JEXEC') || die('=;)');
+
+// Import Joomla! Plugin library file
+jimport('joomla.plugin.plugin');
+
+class plgContentKuKuKontent extends JPlugin
+{
+    /**
+     * Plugin...
+     */
+    public function onContentPrepare($context, &$row, &$params, $page = 0)
+    {
+        include_once JPATH_SITE.'/plugins/content/kukukontent/parser/markdown.php';
+//         include_once JPATH_SITE.'/plugins/content/jTextile/textile/smartypants.php';
+
+//         $textile = new Textile;
+
+//         $row->text = '<!-- jTextile -->'."\n".SmartyPants($textile->TextileThis($row->text));
+        $row->text = Markdown($row->text);
+
+        return true;
+
+        include_once JPATH_SITE.'/plugins/content/jTextile/textile/classTextile.php';
+//         include_once JPATH_SITE.'/plugins/content/jTextile/textile/smartypants.php';
+
+        $textile = new Textile;
+
+//         $row->text = '<!-- jTextile -->'."\n".SmartyPants($textile->TextileThis($row->text));
+        $row->text = '<!-- jTextile -->'."\n".$textile->TextileThis($row->text);
+
+        return true;
+    }//function
+}//class
