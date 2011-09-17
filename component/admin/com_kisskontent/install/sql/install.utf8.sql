fbba9da24f15e1983cea2b8ebe64d914431c416e
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-17 14:15:32 -0500

    Fix missing summary in install.sql

--- component/admin/com_kisskontent/install/sql/install.utf8.sql
+++ component/admin/com_kisskontent/install/sql/install.utf8.sql
@@ -9,6 +9,7 @@ CREATE TABLE `#__kisskontent_versions` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `title` varchar(500) DEFAULT NULL,
   `text` text,
+  `summary` varchar(500) NOT NULL,
   `id_user` tinyint(3) unsigned DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
