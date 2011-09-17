4abeb1f4f69aaadd44a9b4f72cc7cd64881576c2
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-17 13:12:35 -0500

    Add search plugin

---
+++ plugin/search/kisskontent/kisskontent.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @package    KISSKontent
+ * @subpackage Base
+ * @author     Nikolai Plath {@link http://nik-it.de}
+ * @author     Created on 16-Sep-2011
+ * @license    GNU/GPL
+ */
+
+//-- No direct access
+defined('_JEXEC') || die('=;)');
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * KISSKontent Search plugin.
+ *
+ * @package    KISSKontent
+ * @subpackage Plugin
+ */
+class plgSearchKISSKontent extends JPlugin
+{
+    /**
+     * Constructor.
+     *
+     * @param object $subject The object to observe
+     * @param array $config  An array that holds the plugin configuration
+     */
+    public function __construct(& $subject, $config)
+    {
+        parent::__construct($subject, $config);
+
+        //Loads the plugin language file:
+        //$this->loadLanguage();
+    }//function
+
+    /**
+     * Sets the checkbox(es) to be diplayed in the Search Only box:
+     * @return array An array of search areas
+     */
+    public function onContentSearchAreas()
+    {
+        return array(
+            'Kontent' => 'Kontent'
+        );
+    }//function
+
+    /**
+     * Example Search method
+     *
+     * The sql must return the following fields that are used in a common display
+     * routine:
+     - title;
+     - href:            link associated with the title;
+     - browsernav    if 1, link opens in a new window, otherwise in the same window;
+     - section        in parenthesis below the title;
+     - text;
+     - created;
+
+     * @param string Target search string
+     * @param string matching option, exact|any|all
+     * @param string ordering option, newest|oldest|popular|alpha|category
+     * @param mixed An array if the search it to be restricted to areas, null if search all
+     *
+     * @return array Search results
+     */
+    public function onContentSearch($text, $phrase = '', $ordering = '', $areas = null)
+    {
+        if(is_array($areas))
+        {
+            if( ! array_intersect($areas, array_keys($this->onContentSearchAreas())))
+            {
+                return array();
+            }
+        }
+
+        $limit = $this->params->def('search_limit', 50);
+
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true);
+
+        switch ($phrase)
+        {
+            case 'exact':
+                $text = $db->Quote('%'.$db->getEscaped($text, true).'%', false);
+
+                $query->where('k.title LIKE '.$text.' OR k.text LIKE '.$text);
+
+                break;
+
+            case 'all':
+            case 'any':
+            default:
+                $words	= explode(' ', $text);
+
+                foreach ($words as $word)
+                {
+                    $word = $db->Quote('%'.$db->getEscaped($word, true).'%', false);
+
+                    $query->where('k.title LIKE '.$word.' OR k.text LIKE '.$word);
+                }//foreach
+
+                break;
+        }
+
+        switch ($ordering)
+        {
+            case 'oldest':
+                $query->order('k.modified ASC');
+                break;
+
+            case 'popular':
+                $query->order('k.modified ASC');
+                break;
+
+            case 'alpha':
+                $query->order('k.title ASC');
+                break;
+
+            case 'category':
+                $query->order('k.modified ASC');
+                break;
+
+            case 'newest':
+            default:
+                $query->order('k.id DESC');
+        }//switch
+
+        $query->from('#__kisskontent AS k');
+        $query->select('k.title, k.text, k.id as created');
+
+        $db->setQuery($query, 0, $limit);
+
+        $rows = $db->loadObjectList();
+
+        foreach ($rows as $row)
+        {
+            $row->href = 'aaa';
+            $row->section = 'Kontent';
+            $row->browsernav = false;
+        }//foreach
+
+        return $rows;
+    }//function
+}//class
---
+++ plugin/search/kisskontent/kisskontent_CHANGELOG.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * CHANGELOG
+ *
+ * This is the changelog for KISSKontent.<br>
+ * <b>Please</b> be patient =;)
+ *
+ * @package    KISSKontent
+ * @subpackage Documentation
+ * @author     Nikolai Plath {@link http://nik-it.de}
+ * @author     Created on 16-Sep-2011
+ */
+
+//--No direct access to this changelog...
+defined('_JEXEC') || die('=;)');
+
+//--For phpDocumentor documentation we need to construct a function ;)
+/**
+ * CHANGELOG
+ * {@source}
+ */
+function CHANGELOG()
+{
+/*
+_______________________________________________
+_______________________________________________
+
+This is the changelog for KISSKontent
+
+Please be patient =;)
+_______________________________________________
+_______________________________________________
+
+Legend:
+
+ * -> Security Fix
+ # -> Bug Fix
+ + -> Addition
+ ^ -> Change
+ - -> Removed
+ ! -> Note
+______________________________________________
+
+16-Sep-2011 Nikolai Plath
+ ! Startup
+
+*/
+}//--This is the END
