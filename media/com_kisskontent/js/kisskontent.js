4e32116ba2e76c4bbe37d59499a4abe9a7f8d699
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-19 17:21:55 -0500

    Add js function for diff

--- media/com_kisskontent/js/kisskontent.js
+++ media/com_kisskontent/js/kisskontent.js
@@ -3,7 +3,9 @@ function kisskontentPreview(baseUri, p)
 	var preview = document.id('kisskontentPreview');
 	var kontent = document.id('kisskontentKontent');
 
-    new Request({
+	preview.set('html', '');
+
+	new Request({
     	url: baseUri + 'index.php?option=com_kisskontent'
     	+ '&task=preview&tmpl=component&format=raw'
     	+ '&p=' + p,
@@ -19,3 +21,31 @@ function kisskontentPreview(baseUri, p)
 		}
 	}).send();
 }//function
+
+function kisskontentDifferences(baseUri, p)
+{
+	var preview = document.id('kisskontentPreview');
+	var kontent = document.id('kisskontentKontent');
+	
+	var diffAll =(document.id('chkDiffAll').checked) ? '&diffAll=1' : '';
+	
+	preview.set('html', '');
+
+    new Request({
+    	url: baseUri + 'index.php?option=com_kisskontent'
+    	+ '&task=differences&tmpl=component&format=raw'
+    	+ '&p=' + p
+    	+ diffAll,
+
+		method: 'post',
+		data: 'kontent=' + kontent.value,
+		
+		//onRequest..
+		
+		onSuccess: function(response) {
+			preview.set('html', response);
+			preview.set('class', 'kisskuntentPreview');
+		}
+	}).send();
+}//function
+
