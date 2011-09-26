1064522729d221289e7e5a71444395282072f3d3
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-26 13:48:45 -0500

    Update install/uninstall sql

--- component/admin/com_kisskontent/install/sql/install.utf8.sql
+++ component/admin/com_kisskontent/install/sql/install.utf8.sql
@@ -1,16 +1,28 @@
 CREATE TABLE `#__kisskontent` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(500) DEFAULT NULL,
   `text` text,
   PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `#__kisskontent_versions` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `title` varchar(500) DEFAULT NULL,
-  `text` text,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(500) NOT NULL,
+  `id_kiss` int(10) unsigned NOT NULL COMMENT 'Kontent ID',
+  `text` text NOT NULL,
   `summary` varchar(500) NOT NULL,
-  `id_user` tinyint(3) unsigned DEFAULT NULL,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_user` tinyint(3) unsigned NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Modified date/time',
+  `lang` varchar(5) NOT NULL COMMENT 'Language Tag',
   PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__kiss__translations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+  `id_kiss` tinyint(3) unsigned NOT NULL COMMENT 'KISSKontent ID',
+  `title` varchar(500) NOT NULL COMMENT 'Translation title',
+  `text` text NOT NULL COMMENT 'Translation text',
+  `lang` varchar(5) NOT NULL COMMENT 'Language tag',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Translations for KISSKontent';
+
--- component/admin/com_kisskontent/install/sql/uninstall.utf8.sql
+++ component/admin/com_kisskontent/install/sql/uninstall.utf8.sql
@@ -1,3 +1,5 @@
 DROP TABLE `#__kisskontent`;
 
 DROP TABLE `#__kisskontent_versions`;
+
+DROP TABLE `#__kiss_translations`;
