9d37daf50d41accc08d8ed9c24b0addb73bdec12
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-26 12:41:08 -0500

    Implement nuke, Started a local help system

--- component/site/com_kisskontent/controller.php
+++ component/site/com_kisskontent/controller.php
@@ -24,7 +24,17 @@ class KISSKontentController extends JController
     {
         if( ! KISSKontentHelper::getActions()->get('core.edit'))
         {
-            JError::raiseError(403, jgettext('You are not allowed to edit Kontent pages.'));
+            JError::raiseWarning(403, jgettext('You are not allowed to edit Kontent pages.'));
+        }
+
+        parent::display();
+    }//function
+
+    public function translate()
+    {
+        if( ! KISSKontentHelper::getActions()->get('core.translate'))
+        {
+            JError::raiseWarning(403, jgettext('You are not allowed to translate Kontent pages.'));
         }
 
         parent::display();
@@ -50,8 +60,6 @@ class KISSKontentController extends JController
 
     public function dotranslate()
     {
-        var_dump($_REQUEST);
-
         JRequest::checkToken() || jexit(jgettext('Invalid token'));
 
         try
@@ -162,22 +170,48 @@ class KISSKontentController extends JController
         echo $o->text;
     }//function
 
+    public function listkontent()
+    {
+        ;
+    }
+
     public function nukeKonfirmed()
     {
         $this->nuke(true);
     }//function
 
-    public function nuke($confirmed = false)
+    public function nuke($konfirmed = false)
     {
-        var_dump($_REQUEST);
 
         try
         {
             $model = $this->getModel();
 
-            $model->nuke($confirmed);
+            $toNuke = $model->nuke($konfirmed);
 
-            JFactory::getApplication()->enqueueMessage(jgettext('Your Kontent has been nuked'));
+            if( ! $konfirmed)
+            {
+                echo '<h1>'.sprintf(jgettext('Do you really want to...')).'</h1>';
+
+                echo sprintf(jgettext('Delete the Kontent item: %s'), $toNuke->kiss->title).'<br />';
+
+                echo sprintf(jngettext('Delete %d version (ID: %s)', 'Delete %d versions (IDs: %s)', count($toNuke->versions))
+                ,count($toNuke->versions), implode(', ', $toNuke->versions)).'<br />';
+
+                echo sprintf(jngettext('Delete %d translation (ID: %s)', 'Delete %d translations (IDs: %s)', count($toNuke->translations))
+                ,count($toNuke->translations), implode(', ', $toNuke->translations)).'<br />';
+
+                echo JHtml::link(JRoute::_('&task=nukeKonfirmed')
+                , jgettext('Yes I really want to nuke this Kontent')
+                , array('style' => 'font-weight: bold; color: red;'));
+                //         var_dump($_REQUEST);
+
+                //                 var_dump($toNuke);
+            }
+            else
+            {
+                JFactory::getApplication()->enqueueMessage(jgettext('Your Kontent has been nuked'));
+            }
         }
         catch (Exception $e)
         {
@@ -187,5 +221,106 @@ class KISSKontentController extends JController
         JRequest::setVar('view', 'listkontent');
 
         parent::display();
+    }//function
+
+    public function help()
+    {
+        $defaultLang = 'en-GB';
+
+        if(KISS_ML)
+        {
+            $lang = g11n::getDefault();
+            $lang =($lang) ?: $defaultLang;
+        }
+
+        $lang =($lang) ?: $defaultLang;
+
+        $file = JPATH_COMPONENT_SITE.'/demo/help_'.$lang.'.md';
+
+        if( ! JFile::exists($file))
+        {
+
+            $lang = $defaultLang;
+
+            $file = JPATH_COMPONENT_SITE.'/demo/help_'.$defaultLang.'.md';
+
+            if( ! JFile::exists($file))
+            {
+                echo 'No help available';
+
+                return;
+            }
+        }
+
+        $p = null;
+
+        $raw = JFile::read($file);
+
+        $parts = explode("\n", $raw);
+
+        $chapters = array();
+
+        $t = '';
+
+        foreach ($parts as $part)
+        {
+            if(preg_match('/^# (\w+)/', $part, $matches))
+            {
+                $t = $matches[1];
+                $chapters[$t] = array();
+
+                continue;
+            }
+
+            if( ! isset($chapters[$t]))
+            continue;//smthg went wrong..
+
+            $chapters[$t][] = $part;
+        }//foreach
+
+        JPluginHelper::importPlugin('content');
+
+        $dispatcher = JDispatcher::getInstance();
+
+        $o = new stdClass;
+
+        $html = array();
+
+        foreach ($chapters as $title => $chapter)
+        {
+            $html[] = '<h1>'.$title.'</h1>';
+
+            $html[] = '<table border="1">';
+            $html[] = '<tr><th>a</th><th>b</th><th>c</th></tr>';
+
+            foreach ($chapter as $line)
+            {
+                $html[] = '<tr>';
+
+                $html[] = '<td style="font-family: monospace;">';
+                $html[] = $line;
+                $html[] = '</td>';
+
+                $o->text = $line;
+                $o->text = KISSKontentHelper::preParse($o->text);
+                $dispatcher->trigger('onContentPrepare'
+                , array('text', &$o, &$p));
+
+                $html[] = '<td nowrap="nowrap">';
+                $html[] = $o->text;
+                $html[] = '</td>';
+
+                $html[] = '<td style="font-family: monospace;">';
+                $html[] = htmlentities($o->text);
+                $html[] = '</td>';
+
+                $html[] = '</tr>';
+            }
+            $html[] = '</table>';
+        }//foreach
+
+        echo implode("\n", $html);
+
+        exit;
     }//function
 }//class
