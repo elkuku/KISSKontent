5348b82429efc71af15729fc9e325b4b17d3bdad
Nikolai Plath <der.el.kuku@gmail.com>
2011-09-27 13:43:42 -0500

    debug and ml switch

--- component/site/com_kisskontent/kisskontent.php
+++ component/site/com_kisskontent/kisskontent.php
@@ -10,57 +10,56 @@
 //-- No direct access
 defined('_JEXEC') || die('=;)');
 
-JHtml::_('stylesheet'//@todo debug
-, 'com_kisskontent/debug.css', array(), true);
-
-jimport('kuku.util');//@todo debug
-
-define('KISS_DBG', 1);
+define('KISS_DBG', 0);//@@DEBUG
 
 defined('NL') || define('NL', "\n");
+
+if(KISS_DBG) JHtml::_('stylesheet'//@@DEBUG
+, 'com_kisskontent/debug.css', array(), true);
+
+if(KISS_DBG) jimport('kuku.util');//@@DEBUG
 
 jimport('joomla.application.component.controller');
 
 $params = JFactory::getApplication('site')->getParams('com_kisskontent');
 
 //-- Handle multilanguage functionality
-try
-{
-    //-- Load the special Language
-
-    if( ! jimport('g11n.language'))
-    {
-        //-- Load dummy language handler -> english only !
-        JLoader::import('g11n_dummy', JPATH_COMPONENT_ADMINISTRATOR.'/helpers');
-
-        //         ecrScript('g11n_dummy');
-        //         ecrScript('php2js');
-    }
-    else
-    {
-        //TEMP@@debug
-        if(0)
-        {
-            g11n::cleanStorage();//@@DEBUG
-            g11n::setDebug(1);
-        }
-
-        //-- Get our special language file
-        g11n::loadLanguage();
-    }
-
-}
-catch(Exception $e)
-{
-    JError::raiseWarning(0, $e->getMessage());
-}//try
-
 if( ! $params->get('kiss_ml', 0))
 {
     define('KISS_ML', 0);
 }
 else
 {
+    try
+    {
+        //-- Load the special Language
+
+        if( ! jimport('g11n.language'))
+        {
+            //-- Load dummy language handler -> english only !
+            JLoader::import('g11n_dummy', JPATH_COMPONENT_ADMINISTRATOR.'/helpers');
+
+            //         ecrScript('g11n_dummy');
+            //         ecrScript('php2js');
+        }
+        else
+        {
+            if(0)//@@DEBUG
+            {
+                g11n::cleanStorage();
+                g11n::setDebug(1);
+            }
+
+            //-- Get our special language file
+            g11n::loadLanguage();
+        }
+
+    }
+    catch(Exception $e)
+    {
+        JError::raiseWarning(0, $e->getMessage());
+    }//try
+
     if( ! class_exists('g11n'))
     {
         echo 'Please install the <a href="...">g11n language library</a> to use the multilanguage functionality.';
@@ -88,4 +87,4 @@ $controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
 
 // g11n::debugPrintTranslateds();
-echo KuKuUtility::dump();
+if(KISS_DBG) echo KuKuUtility::dump();
